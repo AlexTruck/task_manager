a62559d0d2010b60ef9713cddd3297cc16413c05
AlexTruck <alexandr.kujba@gmail.com>
2015-08-14 18:24:00 +0300

    Previously added basic mailing functional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to Task Manager!"
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
       redirect_to @user
     else
       render 'new'
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150812125527) do
+ActiveRecord::Schema.define(version: 20150814124749) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.string   "password_digest"
+    t.string   "activation_digest"
+    t.boolean  "activated",             default: false
+    t.datetime "activated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
